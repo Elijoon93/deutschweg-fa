477c9e0e3832d9bb77eb19ceec50ce25fe6e2435
Elijoon93 <abtin412@gmail.com>
2026-07-23 14:55:11 +0330

    git add . git commit -m "refactor(ux): simplify DeutschWeg into four-tab coach flow in X16.2" git push origin main

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='deutschweg-x16-1-guided-ux-v1';
+const CACHE='deutschweg-x16-2-coach-flow';
 const ASSETS=['./','./index.html','./offline.html','./manifest.webmanifest','./app-config.js','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
